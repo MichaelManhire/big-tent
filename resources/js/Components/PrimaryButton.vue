b651d6030cc44895c0b8b9c32bba1e7257830d8f
Michael Manhire <michaelmanhire@gmail.com>
2021-02-27 13:18:54 -0800

    Add primary button component

---
+++ resources/js/Components/PrimaryButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" :type="type">
+        <slot />
+    </button>
+</template>
+
+<script>
+    export default {
+        props: {
+            type: {
+                type: String,
+                default: 'button',
+            },
+        },
+    }
+</script>
--- resources/js/Pages/Groups/Show.vue
+++ resources/js/Pages/Groups/Show.vue
@@ -11,9 +11,9 @@
                 </div>
             </div>
             <div class="mt-6 flex justify-stretch sm:justify-end md:mt-0">
-                <button class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500" type="button">
+                <primary-button>
                     Join Group
-                </button>
+                </primary-button>
             </div>
         </section>
 
